1006503714e53699606aad7680ccb878ec43cbd7
Ana Maria Trujillo <anamaria.7mp@gmail.com>
2021-09-29 16:40:20 -0500

    remover import innecesarios

--- src/main/java/com/Tienda_Viajes/controller/UsuarioControlador.java
+++ src/main/java/com/Tienda_Viajes/controller/UsuarioControlador.java
@@ -9,10 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
 import com.Tienda_Viajes.interfaceService.IPersonaService;
 import com.Tienda_Viajes.model.Persona;
 
